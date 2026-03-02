589e999456c0cea2a981e51d8ea18b43e21e2a2d
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-03-02 16:50:12 +0530

    revision

---
+++ day37(27.02.10)/rev.js
@@ -0,0 +1,40 @@
+
+// bubbleSort
+// let arr = [4,8,9,6,7]
+
+// function bubbleSort(){
+//     for(let i = 0;i<arr.length -1 ; i++){
+//         for(let j=0 ; j<arr.length-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 //swap
+//                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+//             }
+//         }
+//     }
+//     return arr ;
+// }
+
+// console.log(bubbleSort());
+
+
+//Selection Sort
+
+let arr = [4,3,5,2,1]
+
+// function selectionSort(){
+//     for( let i =0;i<arr.length-1 ;i++){
+//         let minArr = i
+
+//         for(let j=i+1; j<arr.length;j++){
+//             if(arr[j]<arr[minArr]){
+//                 minArr=j
+//             }
+//         }
+
+//         [arr[i], arr[minArr]] = [arr[minArr], arr[i]];
+//     }
+
+//     return arr;
+// }
+
+// console.log(selectionSort());
