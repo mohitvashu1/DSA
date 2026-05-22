d82e59e6ddfd45fa50681a7cbdd13d2891206b43
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-05-22 23:01:01 +0530

    Pattern

---
+++ JAVA DSA/day2/Pattern1.java
@@ -0,0 +1,24 @@
+
+
+public class Pattern1 {
+        
+
+public static void starPrint1(){
+    for(var row =1 ;row<=4; row++){
+         var star ="";
+        for(var col=1 ;col<=4 ; col++ ){
+               star+=" *";
+        }
+            System.out.println(star);
+    }
+
+}
+
+
+
+public static void main(String[] args) {
+   
+    starPrint1();
+}
+    
+}
---
+++ JAVA DSA/day2/Pattern2.java
@@ -0,0 +1,25 @@
+
+
+public class Pattern2 {
+    public static void starPrint1(){
+        
+    for(var row =1 ;row<=5; row++){
+         var star ="";
+        for(var col=1 ;col<=row ; col++ ){
+               star+="* ";
+        }
+            System.out.println(star);
+    }
+    
+}
+
+
+
+
+
+public static void main(String[] args) {
+    starPrint1();
+   
+}
+    
+}
