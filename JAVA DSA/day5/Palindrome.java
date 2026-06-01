b0662fc2225b0a6a2e6e39fdf4c06c952aed1081
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-06-01 23:43:03 +0530

    Five

--- JAVA DSA/day5/Palindrome.java
+++ JAVA DSA/day5/Palindrome.java
@@ -1,5 +1,25 @@
-
-
 public class Palindrome {
+   public static boolean CheckNum(int num){
+    int ans=0;
+    int original=num;
+
     
+    while (num>0) {
+        int rem =num%10;
+        ans=(ans*10)+rem;
+        num=num/10;
+        
+    }
+
+    if(ans==original){
+        return true;
+    }else{
+        return false;
+    }
+   }
+    public static void main(String[] args) {
+        int num = 12321;
+        boolean result=CheckNum(num);
+        System.out.println(result);
+    }
 }
